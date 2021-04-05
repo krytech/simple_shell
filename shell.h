3734dffead9cddfa62ad18f300f1bb5c0c8ecd52
Carson Stearn <cstearn@gmail.com>
2021-04-05 19:14:16 +0000

    adding .h file

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h> /* malloc, free */
+#include <string.h>
+#include <sys/types.h> /* open */
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+/* helper functions */
+
+
+/* our functions */
+
+
+/* error functions */
+
+
+#endif
