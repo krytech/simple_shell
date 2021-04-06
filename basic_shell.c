b0120dd553119b147d1ad0551af091dc0559f259
keysmusician <justin.masayda@gmail.com>
2021-04-06 18:44:53 +0000

    Move string splitting out of child process to fix mem leaks

--- basic_shell.c
+++ basic_shell.c
@@ -6,7 +6,7 @@ int main(void)
 	size_t len = 0;
 	pid_t pid;
 	char *buffer = NULL;
-	str_list_t *child_CLAs;
+	str_list_t *child_CLAs = NULL;
 	char **argv = NULL;
 	char *prmpt = "Holberton Shell$";
 
@@ -16,15 +16,15 @@ int main(void)
 		getl_r = getline(&buffer, &len, stdin); /* Wait for and store user input */
 		if (getl_r == -1) /* ^D or other failure */
 			break;
-
+		/* Format input for execution */
+		child_CLAs = split_str(buffer, " \n"); /* Create a linked list from the input */
+		argv = str_list_t_to_array(child_CLAs); /* Create argv from the linked list */
 		pid = fork(); /* Create a child process */
 		if (pid == -1)
 			exit(EXIT_FAILURE);
 		else if (pid == 0) /* Inside the child process: */
-		{ /* Format input for execution */
-			child_CLAs = split_str(buffer, " \n"); /* Create a linked list from the input */
-			argv = str_list_t_to_array(child_CLAs); /* Create argv from the linked list */
-			exec = execve(argv[0], argv, NULL); /* Attempt to execute the string as a command */
+		{
+			exec = execve(argv[0], argv, NULL); /* Attempt to execute the string arguments as a command */
 			if (exec == -1)
 			{
 				exit(EXIT_FAILURE);
@@ -32,9 +32,9 @@ int main(void)
 		}
 		if (wait(NULL) == -1) /* Let the child process exit before continuing */
 			exit(EXIT_FAILURE);
+		free_list(child_CLAs);
+		free(argv);
 	}
-	free_list(child_CLAs);
-	free(argv);
 	free(buffer);
 	return (0);
 }
