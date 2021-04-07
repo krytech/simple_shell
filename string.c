1c42cfd72d38406018b6352b0891b4e95351b4b9
Carson Stearn <cstearn@gmail.com>
2021-04-07 19:11:04 +0000

    added strcmp

--- string.c
+++ string.c
@@ -69,3 +69,33 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: the number value of the comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
+}
+
